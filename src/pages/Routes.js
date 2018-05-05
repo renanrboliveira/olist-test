83438e9f7569cbd240452c837b2134118268245c
Renan Oliveira <ren4n.oliveir4@gmail.com>
2018-05-05 10:33:31 -0300

    refactor folder routes

---
+++ src/pages/Routes.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import { publicRoutes } from './pathUrls'
+import NotFoundPage from './notfound'
+
+const Routes = () => {
+  const setRoute = route =>
+    <Route key={route.path} {...route} exact />
+
+  const routes = [
+    publicRoutes.map(setRoute)
+  ]
+
+  return (
+    <Switch>
+      {routes}
+      <Route component={NotFoundPage} />
+    </Switch>
+  )
+}
+
+export default Routes
---
+++ src/pages/pathUrls.js
@@ -0,0 +1,9 @@
+
+import Register from './register'
+
+export const publicRoutes = [
+  {
+    path: '/',
+    component: Register
+  }
+]
