436a4659c9d528a10e17604b9ed7e17f55917134
Renan Oliveira <ren4n.oliveir4@gmail.com>
2018-05-05 16:31:11 -0300

    improve text in input

--- src/components/base/styles/Input.style.js
+++ src/components/base/styles/Input.style.js
@@ -7,6 +7,8 @@ export const InputStyle = styled.input`
   height: 2.75rem;
   box-shadow: inset 0 0.1875rem 0.1875rem rgba(0,0,0,0.05);
   padding: 0.875rem;
+  color: #696D8C;
+  font-size: .9rem;
   &:focus {
     outline: 1px solid ${props => colorByProps(props) === '#EAEAF4' ? '#0A1F9C' : colorByProps(props)};
   }
