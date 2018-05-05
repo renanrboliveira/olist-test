abf350bf8bb6f12383eb6f7c398adfcdbd8c85f2
Renan Oliveira <ren4n.oliveir4@gmail.com>
2018-05-05 15:58:18 -0300

    add normalize polished

--- src/styles/globals.style.js
+++ src/styles/globals.style.js
@@ -1,4 +1,5 @@
 import { injectGlobal } from 'styled-components'
+import { normalize } from 'polished'
 
 import SoleilRegularWoff from './../assets/Soleil-Regular.woff'
 import SoleilRegularEot from './../assets/Soleil-Regular.eot'
@@ -12,6 +13,7 @@ import SoleilBoldttf from './../assets/Soleil-Bold.ttf'
 import SoleilBoldsvg from './../assets/Soleil-Bold.svg'
 
 injectGlobal`
+  ${normalize}
   @font-face {
     font-family: Soleil;
     src: url(${SoleilRegularEot});
@@ -33,178 +35,7 @@ injectGlobal`
   html {
     line-height: 1.5;
     font-size: 100%;
-    -webkit-text-size-adjust: 100%;
   }
-
-  body {
-    margin: 0;
-  }
-
-  h1 {
-    font-size: 2em;
-    margin: 0.67em 0;
-  }
-
-  hr {
-    box-sizing: content-box;
-    height: 0;
-    overflow: visible;
-  }
-
-  pre {
-    font-family: monospace, monospace;
-    font-size: 1em;
-  }
-
-  a {
-    background-color: transparent;
-  }
-
-  abbr[title] {
-    border-bottom: none;
-    text-decoration: underline;
-    text-decoration: underline dotted;
-  }
-
-  b,
-  strong {
-    font-weight: bolder;
-  }
-  code,
-  kbd,
-  samp {
-    font-family: monospace, monospace;
-    font-size: 1em;
-  }
-
-  small {
-    font-size: 80%;
-  }
-
-  sub,
-  sup {
-    font-size: 75%;
-    line-height: 0;
-    position: relative;
-    vertical-align: baseline;
-  }
-
-  sub {
-    bottom: -0.25em;
-  }
-
-  sup {
-    top: -0.5em;
-  }
-  img {
-    border-style: none;
-  }
-
-  button,
-  input,
-  optgroup,
-  select,
-  textarea {
-    font-family: inherit;
-    font-size: 100%;
-    line-height: 1.5;
-    margin: 0;
-  }
-
-  button,
-  input {
-    overflow: visible;
-  }
-
-  button,
-  select {
-    text-transform: none;
-  }
-
-  button,
-  [type="button"],
-  [type="reset"],
-  [type="submit"] {
-    -webkit-appearance: button;
-  }
-
-  button::-moz-focus-inner,
-  [type="button"]::-moz-focus-inner,
-  [type="reset"]::-moz-focus-inner,
-  [type="submit"]::-moz-focus-inner {
-    border-style: none;
-    padding: 0;
-  }
-
-  button:-moz-focusring,
-  [type="button"]:-moz-focusring,
-  [type="reset"]:-moz-focusring,
-  [type="submit"]:-moz-focusring {
-    outline: 1px dotted ButtonText;
-  }
-
-  fieldset {
-    padding: 0.35em 0.75em 0.625em;
-  }
-
-  legend {
-    box-sizing: border-box;
-    color: inherit;
-    display: table;
-    max-width: 100%;
-    padding: 0;
-    white-space: normal;
-  }
-  progress {
-    vertical-align: baseline;
-  }
-
-  textarea {
-    overflow: auto;
-  }
-
-  [type="checkbox"],
-  [type="radio"] {
-    box-sizing: border-box;
-    padding: 0;
-  }
-
-  [type="number"]::-webkit-inner-spin-button,
-  [type="number"]::-webkit-outer-spin-button {
-    height: auto;
-  }
-
-  [type="search"] {
-    -webkit-appearance: textfield;
-    outline-offset: -2px;
-  }
-
-  [type="search"]::-webkit-search-decoration {
-    -webkit-appearance: none;
-  }
-
-  ::-webkit-file-upload-button {
-    -webkit-appearance: button;
-    font: inherit;
-  }
-
-
-  details {
-    display: block;
-  }
-
-  summary {
-    display: list-item;
-  }
-
-  template {
-    display: none;
-  }
-
-  [hidden] {
-    display: none;
-  }
-
   html, body, body > #app {
     font-family: Soleil, sans-serif;
     color: #696D8C;
